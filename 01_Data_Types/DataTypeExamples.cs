54a65f7413a6278a7d812bbda69f57f73856e7a8
Joshua Tucker <thejltucker@gmail.com>
2019-04-19 11:11:56 -0400

    Boolean Walkthrough

--- 01_Data_Types/DataTypeExamples.cs
+++ 01_Data_Types/DataTypeExamples.cs
@@ -61,11 +61,57 @@ namespace _01_Data_Types
             //-- This means ulong covers the range from 0 to 18,446,744,073,709,551,615
             ulong ulongLow = 0;
             ulong ulongHigh = 18446744073709551615;
-            Console.WriteLine($"An unsigned long, or ulong, will range from {ulongLow} to {ulongHigh}.");
+            Console.WriteLine($"An unsigned long, or ulong, will range from {ulongLow} to {ulongHigh}.\n");
             #endregion Whole Number Types
 
-            Console.WriteLine();
             //-- All of these are great if we have whole numbers, but if we want something between two integers we'll have to use another type that supports decimal values
+
+            #region Decimal Types
+            //-- The first type that supports decimals we'll discuss is a float, or a 32 bit floating point value
+            //-- Values that support decimals are a little harder to visualize their range, as floats range from ±1.5 x 10^−45 to ±3.4 x 10^38
+            //-- While there are some exceptions, floats generally maintain a precision up to 7 digits
+            //-- Floats have a suffix of f
+            float floatExample = 3.141592653589793238462643383279f; //This value, when used, will have a value of 3.512346 as it gets rounded to 7 digits
+            Console.WriteLine($"3.141592653589793238462643383279f gets rounded to {floatExample} consisting of 7 digits.");
+
+            //-- Another type C# has is called double
+            //-- Doubles are 64 bit numbers that cover a range of 5.0 x 10^-324 to 1.7 x 10^308
+            //-- This allows them to be precise up to 15-16 digits on average
+            //-- Doubles can be notated with a suffix of d. If a number with a decimal is not given a suffix, it will default to double
+            double doubleExample = 3.141592653589793238462643383279d;
+            Console.WriteLine($"3.141592653589793238462643383279d will get rounded to {doubleExample} when used. Notice it has 15 total digits.");
+
+            //-- The last decimal type we'll be using will be the decimal type
+            //-- Decimals are 128 bit numbers that cover a range of 1.0 x 10^-28 to 7.9 x 10^28
+            //-- Generally this allows them to be accurate up to 28-29 decimal places
+            //-- Because of the size of a decimal, we only use them when we need a high level of accuracy such as dealing with financial calculations
+            //-- Decimals have the suffix of m
+            decimal decimalExample = 3.141592653589793238462643383279m;
+            Console.WriteLine($"3.141592653589793238462643383279m will get rounded to {decimalExample} when used. Notice it has 28 digits after the decimal.");
+            #endregion Decimal Types
+        }
+
+        /// <summary>
+        /// Here we cover booleans which will have a true false value.
+        /// </summary>
+        [TestMethod]
+        public void Booleans()
+        {
+            //-- Boolean types are declared with the keyword of bool
+            //-- A bool is either going to have the value of true or false
+            bool isTrue = true;
+            bool isFalse = false;
+            Console.WriteLine($"We can have a bool set to {isTrue} as well as {isFalse}.");
+
+            //-- Booleans can also refer to anything with a true false value, not just an instance of the bool type
+            //-- If I make a statement such as "I am 24 years old" then we know that this can either be true or false
+            //-- Let's look at an example
+            int age = 24;
+            bool isAge = (24 == age);
+            bool isNotAge = (24 != age);
+            Console.WriteLine($"Here we can state that my age is 24 and see it is a {isAge} statement. If we try to state that 24 is not our age we see it gives us {isNotAge}.");
+
+            //-- Later on we can see how this interacts with Conditionals
         }
     }
 }
